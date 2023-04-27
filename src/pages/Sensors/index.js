860a3452959bbf93fe9d69b9f0da7de50a300c7d
heitorsimizu <heitor.simizu@gmail.com>
2023-04-26 22:19:10 -0300

    modal sensores

--- src/pages/Sensors/index.js
+++ src/pages/Sensors/index.js
@@ -1,14 +1,47 @@
-import { Text, ScrollView } from 'react-native';
+import { Text, Modal, ScrollView, View, StyleSheet} from 'react-native';
 import { TextInput, Button, Card } from 'react-native-paper';
 import { useState, useEffect } from 'react';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
 
 export default function SensorScreen() {
+    const [visible, setVisible] = useState(false);
+
+    const handleOpenModal = () => {
+        setVisible(true);
+    };
+
+    const handleCloseModal = () => {
+        setVisible(false);
+    };
 
     const [sensors, setSensorList] = useState([
         { id: 1, tipo: 'Acidez', unidade: 'pH', valor: '7,5', minValue: '6,5', maxValue: '8,5' },
         { id: 2, tipo: 'Condutividade Elétrica', unidade: 'μS/cm', valor: '1,2', minValue: '0,5', maxValue: '2,5' },
         { id: 3, tipo: 'Temperatura', unidade: '°C', valor: '25', minValue: '20', maxValue: '30' }
     ]);
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        modal: {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'white',
+            margin: 50,
+            borderRadius: 10,
+            padding: 20,
+        },
+        modalTitle: {
+            fontSize: 20,
+            fontWeight: 'bold',
+            marginBottom: 10,
+        },
+    });
 
     useEffect(
         () => {
@@ -17,8 +50,17 @@ export default function SensorScreen() {
     );
 
     return (
-        <ScrollView>
 
+        <ScrollView>
+            <Button title="Abrir Modal" onPress={handleOpenModal}> Abrir</Button>
+            <Modal visible={visible} animationType="slide">
+                <View style={styles.modal}>
+                    <Text style={styles.modalTitle}>Adicione o id do sensor</Text>
+                    <TextInput></TextInput>
+                    <Button>Salvar</Button>
+                    <Button title="Fechar" onPress={handleCloseModal} >Fechar</Button>
+                </View>
+            </Modal>
             {
                 sensors.map((item, index) => (
                     <Card style={{ margin: 15 }}>
@@ -51,4 +93,5 @@ export default function SensorScreen() {
 
         </ScrollView>
     );
+
 }
