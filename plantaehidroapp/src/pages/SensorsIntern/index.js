69ca167fa9c125b02b9374c3308f065dbf813f4a
Lucas Lima <lucalmendes9@gmail.com>
2023-05-11 02:18:45 -0300

    feat: add dayjs

--- plantaehidroapp/src/pages/SensorsIntern/index.js
+++ plantaehidroapp/src/pages/SensorsIntern/index.js
@@ -2,18 +2,19 @@
 import React, { useContext, useState } from 'react';
 import { View, ScrollView, Dimensions } from 'react-native';
 import { LineChart } from 'react-native-chart-kit';
+import dayjs from 'dayjs';
 import styles from './styles';
 
 export default function SensorInternScreen({ navigation, route }) {
   const data = {
     labels: [
-      '2023-05-09T02:56:28.675Z',
-      '2023-04-27T23:28:03.188Z',
-      '2023-04-26T00:17:18.551Z',
-      '2023-04-25T00:06:52.001Z',
-      '2023-04-25T00:06:17.042Z',
-      '2023-04-24T23:29:29.107Z',
-      '2023-04-24T23:29:14.184Z',
+      dayjs('2023-05-09T02:56:28.675Z').format('HH:mm:ss DD/MM/YY'),
+      dayjs('2023-04-27T23:28:03.188Z').format('HH:mm:ss DD/MM/YY'),
+      dayjs('2023-04-26T00:17:18.551Z').format('HH:mm:ss DD/MM/YY'),
+      dayjs('2023-04-25T00:06:52.001Z').format('HH:mm:ss DD/MM/YY'),
+      dayjs('2023-04-25T00:06:17.042Z').format('HH:mm:ss DD/MM/YY'),
+      dayjs('2023-04-24T23:29:29.107Z').format('HH:mm:ss DD/MM/YY'),
+      dayjs('2023-04-24T23:29:14.184Z').format('HH:mm:ss DD/MM/YY'),
     ],
     datasets: [
       {
