2e1f0ee602e68f781f35bf8d3ee1e4016f70abd4
Lucas Lima <lucalmendes9@gmail.com>
2023-05-22 15:44:17 -0300

    fix: dates gtm

--- plantaehidroapp/src/helper/adjustDate.js
+++ plantaehidroapp/src/helper/adjustDate.js
@@ -2,6 +2,6 @@ import dayjs from 'dayjs';
 
 export default date => {
   return dayjs
-    .tz(dayjs(date).format('YYYY-MM-DD HH:mm') + '+03:00', 'America/Sao_Paulo')
+    .tz(dayjs(date).format('YYYY-MM-DD HH:mm') + '+00:00', 'America/Sao_Paulo')
     .format('HH:mm DD/MM/YYYY');
 };
